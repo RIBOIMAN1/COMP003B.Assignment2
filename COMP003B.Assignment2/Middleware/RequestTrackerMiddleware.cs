736df7645bf8b64f856c898fcc57430991e22ccd
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-03-26 23:42:14 -0700

    Added custom middleware to the ASP.NET core application's request pipeline (adding RequestTrackerMiddleware class from the project name's namespace) and added constructor of this middleware class.

--- COMP003B.Assignment2/Middleware/RequestTrackerMiddleware.cs
+++ COMP003B.Assignment2/Middleware/RequestTrackerMiddleware.cs
@@ -2,5 +2,18 @@
 {
 	public class RequestTrackerMiddleware
 	{
+		private readonly RequestDelegate _next;
+		private readonly ILogger<RequestTrackerMiddleware> _log;
+		public RequestTrackerMiddleware(RequestDelegate next, ILogger<RequestTrackerMiddleware> _log;
+		{
+			_next = next;
+			_log = log;
+		}
+		public async Task InvokeAsync(HttpContent context)
+
+		{
+			_log.LogInfo($"Request the method: {context.Requesting.Method}");
+			_log.LogInfo($"Request the path: {context.Requesting.Path}");
+		}
 	}
 }
--- COMP003B.Assignment2/Program.cs
+++ COMP003B.Assignment2/Program.cs
@@ -27,8 +27,8 @@ namespace COMP003B.Assignment2
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
-            app.UseWelcomePage("/welcome");
+			app.UseMiddleware<COMP003B.Assignment2.Middleware.RequestTrackerMiddleware>();
+			app.UseWelcomePage("/welcome");
 
             app.UseMiddleware<RequestTrackerMiddleware>();
 
