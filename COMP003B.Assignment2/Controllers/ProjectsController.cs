5c6111eebf062917ab72452c4a6c8483f3ffd34a
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-03-28 13:36:24 -0700

    Made the ProjectsController class inherit from the Controller class, as well as linking the views for all of the .cshtml files in the "Projects" folder, linking by file name (Completed, Current, and Ideas).

--- COMP003B.Assignment2/Controllers/ProjectsController.cs
+++ COMP003B.Assignment2/Controllers/ProjectsController.cs
@@ -1,6 +1,24 @@
-﻿namespace COMP003B.Assignment2.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers
 {
-	public class ProjectsController
+	public class ProjectsController : Controller
 	{
+		[HttpGet]
+		public IActionResult Completed()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Current()
+		{
+			return View();
+		}
+		[HttpGet]
+		public IActionResult Ideas()
+		{
+			return View();
+		}
 	}
 }
